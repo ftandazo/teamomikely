ce0035a293d284bafbc870c08374d5fedf23ee15
ftandazo <francisgabriel1403@gmail.com>
2026-03-20 19:01:36 -0500

    Update generar_poema.js

--- generar_poema.js
+++ generar_poema.js
@@ -4,7 +4,7 @@ async function generarPoema() {
   try {
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + process.env.GEMINI_API_KEY,
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent + process.env.GEMINI_API_KEY,
       {
         method: "POST",
         headers: {
